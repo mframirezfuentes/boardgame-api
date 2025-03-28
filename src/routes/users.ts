a0aebd2943ad3f660a85b7ef6f29b0879051e58d
Fernanda Ramírez <mframirezfuentes@gmail.com>
2025-03-28 15:51:33 -0300

    add authenticate to route about user

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -3,8 +3,8 @@ const router = express.Router();
 import authenticateToken from "../middlewares/authMiddlerware";
 import userController from "../controllers/userController";
 
-router.get("/users", userController.getUsers);
-router.post("/users", userController.createUser);
+router.get("/users", authenticateToken, userController.getUsers);
+router.post("/users", authenticateToken, userController.createUser);
 router.put("/users/:id", authenticateToken, userController.updateUser);
 router.delete("/users/:id", authenticateToken, userController.deleteUser);
 
